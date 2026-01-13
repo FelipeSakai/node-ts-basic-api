a3e7cedb31b0a2415922604b80d3d9f60ee369fe
Felipe Sakai <felipesakaiipolito@gmail.com>
2026-01-13 08:34:19 -0400

    feat(transactions): add transaction type definitions and routes for fetching transactions

--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -5,8 +5,26 @@ import { knexInstance } from "../database.js"
 
 export async function transactionRoutes(app: FastifyInstance) {
     app.get('/', async () => {
-        const transactions = await knexInstance('transactions').select('*')
+        const transactions = await knexInstance('transactions').select()
         return { transactions }
+    })
+
+    app.get('/:id', async (request) => {
+        const getTransactionParamsSchema = z.object({
+            id: z.string().uuid(),
+
+        })
+        const { id } = getTransactionParamsSchema.parse(request.params)
+
+        const transaction = await knexInstance('transactions').where('id', id).first()
+
+        return { transaction }
+    })
+
+    app.get('/summary', async () => {
+        const summary = await knexInstance('transactions').sum('amount', { as: 'amount' }).first()
+
+        return { summary }
     })
 
     app.post('/', async (request, reply) => {
